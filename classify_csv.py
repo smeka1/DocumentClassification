540f8c26e98ece8829c252cb7d3ef0ddbd9c245e
Ubuntu <ubuntu@ip-172-31-23-9.us-west-2.compute.internal>
2018-05-07 00:24:39 +0000

    All done

--- classify_csv.py
+++ classify_csv.py
@@ -1,26 +1,22 @@
-def predict():
-    import pandas as pd
-    import numpy as np
+def predict(list):
     from sklearn.feature_extraction.text import TfidfVectorizer
     from sklearn.externals import joblib
-    
-    column_names = ['Content']
-    temp = pd.read_csv('test100.csv', names = column_names,header=None,chunksize=1000)
-    df = pd.concat(temp, ignore_index=True)
-    
+   
     vect = joblib.load('tfidf.pkl')
     dict = joblib.load('dict.pkl')
     #feature_list = vect.get_feature_names()
     loaded_model = joblib.load('linear_svc_doc_clf_model.pkl')
     #indices = np.argsort(loaded_model.coef_['category_id'])
-    text = vect.transform(df['Content'])
-    i =1
+    #list =['hgjjhgj hghjgjghjgh']
+    text = vect.transform(list)
+    
     predicted_IDs = loaded_model.predict(text)
     for ID in predicted_IDs:
         print(dict[ID])
-    
+        return dict[ID]
+
 def main():
-    predict()
+    predict(['Test'])
     
 if __name__== "__main__":
     main()
--- redirect.py
+++ redirect.py
@@ -1,21 +1,41 @@
 
-from flask import Flask,render_template, redirect, url_for, request
+import os.path
+from flask import Flask,render_template, request, jsonify
+import classify_csv as clfpy
+
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
 
-@app.route('/classify',methods = ['POST', 'GET'])
+@app.route('/api')
+def index():
+   return """
+   <html> 
+   <h2>Welcome to Sriram's API!</h2>
+   <body><h3>These are your available endpoints:</h3><br/>
+   <a href='/classify'>Run requests to this URI</a> 
+   <br/>   
+   <a href='/classify_form'>Use input field here</a>
+   </body>
+   </html>
+   """
+
+@app.route('/api/classify',methods = ['POST', 'GET'])
 def classify():
    if request.method == 'POST':
-      user = request.form['input']
-      return redirect(url_for('success',name = user))
+      user = request.form['words']
+      if(request.headers.get('Content-Type') == 'text/html'):
+         return "HTML response:\n"+ clfpy.predict([user])
+      else:
+         return "JSON response: "+ clfpy.predict([user]) 
    else:
-      user = request.args.get('input')
-      return redirect(url_for('success',name = user))
+      user = request.args.get('words')
+      if(request.headers.get('Content-Type') == 'text/html'):
+         return "HTML response:\n"+ clfpy.predict([user])
+      else:
+         return jsonify (prediction=clfpy.predict([user]))
+      
 
-@app.route('/classifyDoc')
+@app.route('/api/classify_form')
 def classifyDoc():
    return render_template('index.html')
 
